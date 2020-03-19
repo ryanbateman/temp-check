be1c0350276e547fc2fc591c875f876e028a3c79
Ryan Bateman <secretdark@gmail.com>
2020-03-19 18:16:15 +0100

    First check of build trigger

---
+++ routes/sms.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+var router = express.Router();
+
+router.post('/sms', (req, res) => {
+    const twiml = new MessagingResponse();
+
+    const message = twiml.message();
+    message.body('The Robots are coming! Head for the hills!');
+    message.media('https://farm8.staticflickr.com/7090/6941316406_80b4d6d50e_z_d.jpg');
+
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
+});
