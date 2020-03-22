2740500c4763bd3aeba099726b5bc777e73a2ccd
Ryan Bateman <secretdark@gmail.com>
2020-03-22 15:51:40 +0100

    Call/response with whatever has been SMSed by the user

--- routes/sms.js
+++ routes/sms.js
@@ -4,10 +4,13 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 var router = express.Router();
 
 router.post('/', (req, res) => {
+
+    var body = req.body.Body;
+
     const twiml = new MessagingResponse();
 
     const message = twiml.message();
-    message.body('The Robots are coming! Head for the hills!');
+    message.body(`Your temperature is ${body}`);
     message.media('https://farm8.staticflickr.com/7090/6941316406_80b4d6d50e_z_d.jpg');
 
     res.writeHead(200, {'Content-Type': 'text/xml'});
